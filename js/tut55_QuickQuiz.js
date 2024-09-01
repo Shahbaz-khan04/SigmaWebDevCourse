49997137ba9cd44fb0dba1abad3437fc3c4a4a5c
Shahbaz-khan04 <kingshahbaz0313@gmail.com>
2024-09-02 02:22:22 +0500

    55 61 complete

---
+++ js/tut55_QuickQuiz.js
@@ -0,0 +1,12 @@
+let myName = prompt("What is your Name?")
+let myPhoneNum = prompt("What is your Phone Number?")
+let myMarks = prompt("What are your marks?")
+
+let studentDetails = {
+    name : myName,
+    phoneNum : myPhoneNum,
+    marks : myMarks
+}
+
+console.log(studentDetails)
+console.log(typeof studentDetails)
